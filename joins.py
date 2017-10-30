9bf36c198b664c436116a60a12bc005ccf9a9463
Gilles Pilon <gilles.pilon@gillespilon.ca>
2017-10-30 15:52:09 -0500

    beautify merge code

--- joins.py
+++ joins.py
@@ -4,7 +4,12 @@ import pandas as pd
 left_table = pd.read_csv('left_table.csv') # prefer csv for very large files
 left_table.insert(loc=2, column='Desk Location', value='')
 right_table = pd.read_csv('right_table.csv') # prefer csv for very large files
-left_table['Desk Location'] = left_table[['Employee']]    .merge(right_table[['Names', 'Desk Location']],                         left_on='Employee',                          right_on='Names',                          how='left')['Desk Location'].values
+left_table['Desk Location'] = left_table[['Employee']]\
+        .merge(right_table[['Names', 'Desk Location']],\
+        left_on='Employee',\
+        right_on='Names',\
+        how='left')\
+        ['Desk Location'].values
 left_table = left_table[['ID',                          'Employee',                          'Desk Location']]                       [left_table['Desk Location'].isnull()]
 left_table.dropna(subset=['Employee'], inplace=True)
 left_table['ID'] = left_table['ID'].astype(int)
