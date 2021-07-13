51a64fec8bfe81d6b930d2c38bd0835cf6fe92ae
Gilles Pilon <gilles.pilon@gillespilon.ca>
2021-07-12 19:45:35 -0500

    Update astype

--- joins.py
+++ joins.py
@@ -13,6 +13,6 @@ left_table['Desk Location'] = left_table[['Employee']]\
 left_table = left_table[['ID', 'Employee', 'Desk Location']]\
         [left_table['Desk Location'].isnull()]
 left_table = left_table.dropna(subset=['Employee'])
-left_table['ID'] = left_table['ID'].astype(int)
+left_table['ID'] = left_table['ID'].astype(dtype='int')
 left_table.to_csv('left_table_exceptions.csv')
 
