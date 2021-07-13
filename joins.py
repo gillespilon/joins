34f54e5853815e7db34518c5a31b68121774ae7b
Gilles Pilon <gilles.pilon@gillespilon.ca>
2021-07-12 19:58:55 -0500

    Refactor

--- joins.py
+++ joins.py
@@ -1,18 +1,19 @@
 #! /usr/bin/env python3
 
 import pandas as pd
-left_table = pd.read_csv('left_table.csv') # prefer csv for very large files
+left_table = pd.read_csv('left_table.csv')  # prefer csv for very large files
 left_table.insert(loc=2, column='Desk Location', value='')
-right_table = pd.read_csv('right_table.csv') # prefer csv for very large files
+right_table = pd.read_csv('right_table.csv')  # prefer csv for very large files
 left_table['Desk Location'] = left_table[['Employee']]\
-        .merge(right_table[['Names', 'Desk Location']],\
-        left_on='Employee',\
-        right_on='Names',\
-        how='left')\
-        ['Desk Location'].values
-left_table = left_table[['ID', 'Employee', 'Desk Location']]\
-        [left_table['Desk Location'].isnull()]
-left_table = left_table.dropna(subset=['Employee'])
-left_table['ID'] = left_table['ID'].astype(dtype='int')
-left_table.to_csv('left_table_exceptions.csv')
-
+        .merge(
+            right=right_table[['Names', 'Desk Location']],
+            left_on='Employee',
+            right_on='Names',
+            how='left')['Desk Location'].values
+left_table = left_table[
+        ['ID', 'Employee', 'Desk Location']
+    ][left_table['Desk Location']
+        .isnull()]\
+    .dropna(subset=['Employee'])\
+    .astype({'ID': 'int'})\
+    .to_csv('left_table_exceptions.csv')
