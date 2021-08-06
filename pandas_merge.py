10079ab2035262acbb8a1cf9dd4e325b8e644856
Gilles Pilon <gilles.pilon@gillespilon.ca>
2021-08-06 16:51:06 -0500

    Add df_three, merge with df_one_two

--- pandas_merge.py
+++ pandas_merge.py
@@ -6,7 +6,6 @@ Examples of pandas merge
 '''
 
 from pathlib import Path
-import time
 
 from tabulate import tabulate
 import datasense as ds
@@ -42,7 +41,8 @@ def main():
             headers='keys',
             tablefmt='html',
             numalign="right",
-            stralign="right"
+            stralign="right",
+            floatfmt=".2f"
         )
     )
     print()
@@ -52,7 +52,7 @@ def main():
             'ticket': [1001, 1002, 1003, 1004, 1005, 1006, 1007],
             'amount': [24.1, np.NaN, 34.5, 19.5, 26.2, 27.3, np.NaN]
         }
-    )
+    ).astype({'ticket': 'Int64'})
     print('df_two')
     print("- has multiple values in 'id'")
     print()
@@ -62,12 +62,36 @@ def main():
             headers='keys',
             tablefmt='html',
             numalign="right",
-            stralign="right"
+            stralign="right",
+            floatfmt=".2f"
+        )
+    )
+    print()
+    df_three = pd.DataFrame(
+        {
+            'id': [4, 4, 4],
+            'ticket': [1002, 1006, 1007],
+            'amount': [13.69, 11.11, 69.13]
+        }
+    ).astype({'ticket': 'Int64'})
+    print('df_three')
+    print("- has multiple values in 'id'")
+    print("- has unique values in 'ticket'")
+    print()
+    print(
+        tabulate(
+            df_three,
+            headers='keys',
+            tablefmt='html',
+            numalign="right",
+            stralign="right",
+            floatfmt=".2f"
         )
     )
     print()
     # left merge
     # fill_values = {'age': np.NaN, 'ticket': np.NaN}
+    ds.page_break()
     df_one_two = df_one.merge(
         right=df_two,
         how='left',
@@ -80,13 +104,13 @@ def main():
             'age': 'Int64',
             'ticket': 'Int64'
         }
-    # ).fillna(value=fill_values)
     )
     ds.save_file(
         df=df_one_two,
-        file_name='justatest.csv'
+        file_name=Path.cwd() / 'df_one_two.csv'
     )
     print('df_one -> df_two is a one-to-many left merge')
+    print("left_on 'id', right_on 'id'")
     print()
     # print(df)
     print(
@@ -95,7 +119,69 @@ def main():
             headers='keys',
             tablefmt='html',
             numalign="right",
-            stralign="right"
+            stralign="right",
+            floatfmt=".2f"
+        )
+    )
+    print()
+    ds.page_break()
+    print('df_one_two -> df_three is a one-to-one left merge')
+    print("left_on ['id', 'ticket']")
+    print("right_on ['id', 'ticket']")
+    print()
+    df_one_two_three = df_one_two\
+        .drop(columns=['_merge'])\
+        .merge(
+            right=df_three,
+            how='left',
+            left_on=['id', 'ticket'],
+            right_on=['id', 'ticket'],
+            suffixes=('_left', '_right'),
+            indicator=True,
+            validate='one_to_one'
+        )
+    print(
+        tabulate(
+            df_one_two_three,
+            headers='keys',
+            tablefmt='html',
+            numalign="right",
+            stralign="right",
+            floatfmt=".2f"
+        )
+    )
+    print()
+    df_one_two_three = df_one_two_three.drop(columns=['_merge'])
+    df_one_two_three['amount'] = df_one_two_three['amount_left']\
+        .where(
+            df_one_two_three['amount_left'].notnull(),
+            df_one_two_three['amount_right']
+        )
+    print(
+        tabulate(
+            df_one_two_three,
+            headers='keys',
+            tablefmt='html',
+            numalign="right",
+            stralign="right",
+            floatfmt=".2f"
+        )
+    )
+    print()
+    df_one_two_three = df_one_two_three\
+        .drop(columns=['amount_left', 'amount_right'])
+    ds.save_file(
+        df=df_one_two_three,
+        file_name=Path.cwd() / 'df_one_two_three.csv'
+    )
+    print(
+        tabulate(
+            df_one_two_three,
+            headers='keys',
+            tablefmt='html',
+            numalign="right",
+            stralign="right",
+            floatfmt=".2f"
         )
     )
     print()
