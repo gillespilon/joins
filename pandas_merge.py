4bee27a0003ea682360a801069ac7954108812d7
Gilles Pilon <gilles.pilon@gillespilon.ca>
2021-08-07 09:46:13 -0500

    Refactor a little, tidy a lot

--- pandas_merge.py
+++ pandas_merge.py
@@ -1,8 +1,9 @@
 #! /usr/bin/env python3
 '''
-Examples of pandas merge
+Left merge of three dataframes
 
-- Left merge
+df_one_two = df_one <- df_two
+df_one_two_three = df_one_two <- df_three
 '''
 
 from pathlib import Path
@@ -22,9 +23,7 @@ def main():
         header_title=header_title,
         header_id=header_id
     )
-    # print('</pre>')
-    # print(s1)
-    # print('<pre style="white-space: pre-wrap;">')
+    # df_one has unique values in 'id'
     df_one = pd.DataFrame(
         {
             'id': [1, 2, 3, 4, 5],
@@ -32,17 +31,16 @@ def main():
             'ctg': ['A', 'A', 'B', 'C', None]
         }
     ).astype({'age': 'Int64'})
-    print('df_one')
-    print("- has unique values in 'id'")
-    print()
+    df_one.index.name = 'rows'
+    # df_one has unique values in 'id'
     print(
         tabulate(
             df_one,
             headers='keys',
             tablefmt='html',
-            numalign="right",
-            stralign="right",
-            floatfmt=".2f"
+            numalign='right',
+            stralign='right',
+            floatfmt='.2f'
         )
     )
     print()
@@ -53,17 +51,16 @@ def main():
             'amount': [24.1, np.NaN, 34.5, 19.5, 26.2, 27.3, np.NaN]
         }
     ).astype({'ticket': 'Int64'})
-    print('df_two')
-    print("- has multiple values in 'id'")
-    print()
+    df_two.index.name = 'rows'
+    # df_two has multiple values in 'id'
     print(
         tabulate(
             df_two,
             headers='keys',
             tablefmt='html',
-            numalign="right",
-            stralign="right",
-            floatfmt=".2f"
+            numalign='right',
+            stralign='right',
+            floatfmt='.2f'
         )
     )
     print()
@@ -74,23 +71,19 @@ def main():
             'amount': [13.69, 11.11, 69.13]
         }
     ).astype({'ticket': 'Int64'})
-    print('df_three')
-    print("- has multiple values in 'id'")
-    print("- has unique values in 'ticket'")
-    print()
+    df_three.index.name = 'rows'
+    # df_three has multiple values in 'id'
     print(
         tabulate(
             df_three,
             headers='keys',
             tablefmt='html',
-            numalign="right",
-            stralign="right",
-            floatfmt=".2f"
+            numalign='right',
+            stralign='right',
+            floatfmt='.2f'
         )
     )
     print()
-    # left merge
-    # fill_values = {'age': np.NaN, 'ticket': np.NaN}
     ds.page_break()
     df_one_two = df_one.merge(
         right=df_two,
@@ -101,33 +94,28 @@ def main():
         validate='one_to_many'
     ).astype(
         dtype={
-            'age': 'Int64',
-            'ticket': 'Int64'
+            'age': 'Int64'
         }
     )
+    df_one_two.index.name = 'rows'
     ds.save_file(
         df=df_one_two,
         file_name=Path.cwd() / 'df_one_two.csv'
     )
-    print('df_one -> df_two is a one-to-many left merge')
-    print("left_on 'id', right_on 'id'")
-    print()
-    # print(df)
+    # df_one_two = df_one <- df_two is a one-to-many left merge
     print(
         tabulate(
             df_one_two,
             headers='keys',
             tablefmt='html',
-            numalign="right",
-            stralign="right",
-            floatfmt=".2f"
+            numalign='right',
+            stralign='right',
+            floatfmt='.2f'
         )
     )
     print()
     ds.page_break()
-    print('df_one_two -> df_three is a one-to-one left merge')
-    print("left_on ['id', 'ticket']")
-    print("right_on ['id', 'ticket']")
+    # df_one_two_three = df_one_two <- df_three is a one-to-one left merge
     print()
     df_one_two_three = df_one_two\
         .drop(columns=['_merge'])\
@@ -140,18 +128,20 @@ def main():
             indicator=True,
             validate='one_to_one'
         )
+    df_one_two_three.index.name = 'rows'
     print(
         tabulate(
             df_one_two_three,
             headers='keys',
             tablefmt='html',
-            numalign="right",
-            stralign="right",
-            floatfmt=".2f"
+            numalign='right',
+            stralign='right',
+            floatfmt='.2f'
         )
     )
     print()
     df_one_two_three = df_one_two_three.drop(columns=['_merge'])
+    # create 'amount' from 'amount_left' and replace np.NaN from 'amount_right'
     df_one_two_three['amount'] = df_one_two_three['amount_left']\
         .where(
             df_one_two_three['amount_left'].notnull(),
@@ -162,9 +152,9 @@ def main():
             df_one_two_three,
             headers='keys',
             tablefmt='html',
-            numalign="right",
-            stralign="right",
-            floatfmt=".2f"
+            numalign='right',
+            stralign='right',
+            floatfmt='.2f'
         )
     )
     print()
@@ -179,9 +169,9 @@ def main():
             df_one_two_three,
             headers='keys',
             tablefmt='html',
-            numalign="right",
-            stralign="right",
-            floatfmt=".2f"
+            numalign='right',
+            stralign='right',
+            floatfmt='.2f'
         )
     )
     print()
